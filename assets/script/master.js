e8057b02c0fcabed7f91bfc2309394c9ff003d62
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2022-12-31 20:51:22 +0200

    train on controls

--- assets/script/master.js
+++ assets/script/master.js
@@ -1,10 +1,24 @@
-gsap.to('.images img', {
-    y: -200,
-    stagger: {
-        amount: 0.1,
-        from: "center",
-        repeat: -1,
-        yoyo: true,
-    },
+const tween = gsap.to('.images img',{
+    y: -100,
+    repeat: -1,
+    yoyo: true,
+});
+const controles = document.querySelector(".controls");
+const play = controles.querySelector('#play');
+const pause = controles.querySelector('#pause');
+const reverse = controles.querySelector('#reverse');
+const restart = controles.querySelector('#restart');
 
+
+play.addEventListener('click', ()=> {
+    tween.play();
+})
+pause.addEventListener('click', ()=> {
+    tween.pause();
+})
+reverse.addEventListener('click', ()=> {
+    tween.reverse();
+})
+restart.addEventListener('click', ()=> {
+    tween.restart();
 })
