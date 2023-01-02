5accfb5d30e140b067d36e6a1e996608a07da7c6
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2023-01-02 22:35:57 +0200

    work with time line

--- assets/script/master.js
+++ assets/script/master.js
@@ -1,24 +1,21 @@
-const tween = gsap.to('.images img',{
-    y: -100,
-    repeat: -1,
-    yoyo: true,
-});
-const controles = document.querySelector(".controls");
-const play = controles.querySelector('#play');
-const pause = controles.querySelector('#pause');
-const reverse = controles.querySelector('#reverse');
-const restart = controles.querySelector('#restart');
-
-
-play.addEventListener('click', ()=> {
-    tween.play();
-})
-pause.addEventListener('click', ()=> {
-    tween.pause();
-})
-reverse.addEventListener('click', ()=> {
-    tween.reverse();
-})
-restart.addEventListener('click', ()=> {
-    tween.restart();
-})
+gsap.timeline()
+    .from("#app", {
+    opacity: 0,
+    })
+    .from("#app header h1", {
+        duration: 1,
+        scale: 0,
+        opacity: 0,
+        ease: "back",
+    })
+    .from("#app .freds img", {
+        y: 200,
+        opacity: 0,
+        stagger: {
+            amount: 0.5
+        }
+    })
+    .from("#app header .time", {
+        xPercent: 100,
+        ease: 'bounce'
+    })
